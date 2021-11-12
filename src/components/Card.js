44b2ef9b25a896d4278094cffb80fcc19be67787
gab <gabrielfontes321@gmail.com>
2021-11-12 17:10:07 -0300

    Requisito 6

---
+++ src/components/Card.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export default class Card extends Component {
+  render() {
+    const {
+      artistId,
+      artistName,
+      collectionId,
+      collectionName,
+      collectionPrice,
+      artworkUrl100,
+      releaseDate,
+      trackCount,
+    } = this.props;
+    return (
+      <div key={ artistId }>
+        <span>{artistId}</span>
+        <p>{artistName}</p>
+        <p>
+          <Link
+            to={ `/album/${collectionId}` }
+            data-testid={ `link-to-album-${collectionId}` }
+          >
+            Id da coleção
+          </Link>
+        </p>
+        <p>{collectionName}</p>
+        <p>{collectionPrice}</p>
+        <img src={ artworkUrl100 } alt={ artistName } />
+        <p>{releaseDate}</p>
+        <p>{trackCount}</p>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  artistId: PropTypes.number.isRequired,
+  artistName: PropTypes.string.isRequired,
+  collectionId: PropTypes.number.isRequired,
+  collectionName: PropTypes.string.isRequired,
+  collectionPrice: PropTypes.number.isRequired,
+  artworkUrl100: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  trackCount: PropTypes.number.isRequired,
+};
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,104 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
+import Card from '../components/Card';
+
+const INITIAL_STATE = {
+  artistInput: '',
+  artistName: '',
+  onButtonDisabled: true,
+  loading: false,
+  loaded: false,
+  result: [],
+};
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.requestReturn = this.requestReturn.bind(this);
+
+    this.state = {
+      ...INITIAL_STATE,
+    };
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({
+      artistInput: value,
+      artistName: value,
+    }, this.buttonValidator);
+  }
+
+  async requestReturn(artist) {
+    this.setState({
+      loading: true,
+    });
+    const result = await searchAlbumsAPI(artist);
+    this.setState({
+      loading: false,
+      result,
+      artistInput: '',
+      loaded: true,
+    });
+  }
+
+  validatorDisplay() {
+    const { artistName, result } = this.state;
+    if (result.length === 0) return <span>Nenhum álbum foi encontrado</span>;
+    if (result.length > 0) return <p>{`Resultado de álbuns de: ${artistName}`}</p>;
+  }
+
+  buttonValidator() {
+    const { artistInput } = this.state;
+    const minLength = 2;
+    if (artistInput.length >= minLength) {
+      this.setState({
+        onButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        onButtonDisabled: true,
+      });
+    }
+  }
+
   render() {
+    const {
+      artistInput,
+      onButtonDisabled,
+      loading,
+      result,
+      loaded,
+    } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-        <h1>Search</h1>
-      </div>
+      loading ? (<Loading />) : (
+        <div data-testid="page-search">
+          <Header />
+          <form>
+            <label htmlFor="search-artist-button">
+              <input
+                id="search-artist-button"
+                data-testid="search-artist-input"
+                value={ artistInput }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="submit"
+              disabled={ onButtonDisabled }
+              data-testid="search-artist-button"
+              onClick={ () => this.requestReturn(artistInput) }
+            >
+              Pesquisar
+            </button>
+          </form>
+          {loaded && this.validatorDisplay()}
+          {result.map((obj) => <Card key={ obj.artistInput } { ...obj } />)}
+        </div>
+      )
     );
   }
 }
