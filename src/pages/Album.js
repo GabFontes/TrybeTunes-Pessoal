b50452589733c288205d13302ffc0ff85a1ca705
gab <gabrielfontes321@gmail.com>
2021-11-16 21:57:27 -0300

    Requisito 10

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 export default class Album extends Component {
   constructor(props) {
     super(props);
 
+    // this.displayLoading = this.displayLoading.bind(this);
+
     this.state = {
       id: props.match.params.id,
       results: [],
+      loading: false,
+      favorites: []
     };
   }
 
@@ -18,6 +24,20 @@ export default class Album extends Component {
     const { id } = this.state;
     const results = await getMusics(id);
     this.putResultsInState(results);
+    
+    this.setState({
+      loading: true
+    })
+
+    const favorites = await getFavoriteSongs();
+    console.log(favorites);
+
+    this.setState({
+      loading: false,
+      favorites,
+    })
+
+    // this.displayLoading()
   }
 
   putResultsInState(albums) {
@@ -26,25 +46,44 @@ export default class Album extends Component {
     });
   }
 
+  // async displayLoading() {
+  //   this.setState({
+  //     loading: true
+  //   })
+
+  //   const favorites = await getFavoriteSongs();
+
+  //   this.setState({
+  //     loading: false,
+  //     favorites,
+  //   })
+  // }
+
+
   render() {
     const {
       results,
+      loading,
+      favorites,
     } = this.state;
+    console.log(favorites);
     return (
-      <div>
-        <Header />
-        {results.length && (
-          <div data-testid="page-album">
-            <h2 data-testid="artist-name">{results[0].artistName}</h2>
-            <h2 data-testid="album-name">{results[0].collectionName}</h2>
-            {results.map((currentMusic, index) => (
-              currentMusic.previewUrl ? ( // Créditos ao Léo.
-                <MusicCard key={ index } music={ currentMusic } />
-              ) : null
-            ))}
-          </div>
-        )}
-      </div>
+      loading ? (<Loading />) : (
+        <div>
+          <Header />
+          {results.length && (
+            <div data-testid="page-album">
+              <h2 data-testid="artist-name">{results[0].artistName}</h2>
+              <h2 data-testid="album-name">{results[0].collectionName}</h2>
+              {results.map((currentMusic, index) => (
+                currentMusic.previewUrl ? ( // Créditos ao Léo.
+                  <MusicCard key={ index } music={ currentMusic } />
+                ) : null
+              ))}
+            </div>
+          )}
+        </div>
+      )
     );
   }
 }
